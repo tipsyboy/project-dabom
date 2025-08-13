0a5a362e3b67a9b69d54e08979b936791e66b9d7
tipsyboy <gudah13@naver.com>
2025-08-13 15:49:13 +0900

    first commit

---
+++ src/main/java/com/dabom/DabomApplication.java
@@ -0,0 +1,13 @@
+package com.dabom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dabom/DabomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dabom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DabomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
