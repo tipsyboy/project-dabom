9d1214157d470d9993b954a3803de252024e04ee
raccoon-coding <minsungt@naver.com>
2025-08-14 14:22:30 +0900

    [REFACTOR] 의존 관계 수정

--- src/main/java/com/dabom/image/repository/ImageRepository.java
+++ src/main/java/com/dabom/image/repository/ImageRepository.java
@@ -1,8 +1,7 @@
 package com.dabom.image.repository;
 
+import com.dabom.image.model.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.awt.*;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 }
