a245f9bb9ef4fe45047750aac25ef5e216c98680
flionme <sunginkim142@gmail.com>
2025-08-14 16:03:50 +0900

    이미지 업로드 기능 구현 완 이미지 Service 가져오면 누구나 가능!

--- src/main/java/com/dabom/image/controller/ImageController.java
+++ src/main/java/com/dabom/image/controller/ImageController.java
@@ -1,26 +1,38 @@
 package com.dabom.image.controller;
 
+import com.dabom.common.BaseResponse;
+import com.dabom.image.service.ImageService;
+import jakarta.annotation.Resource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController("/image")
+@RequiredArgsConstructor
 public class ImageController {
 
-    private String getContentType(String filename) {
-        String extension = filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
-        switch (extension) {
-            case "jpg":
-            case "jpeg":
-                return "image/jpeg";
-            case "png":
-                return "image/png";
-            case "gif":
-                return "image/gif";
-            case "webp":
-                return "image/webp";
-            default:
-                return "application/octet-stream";
-        }
+    private final ImageService imageService;
+
+    @Value("${file.upload.path}")
+    private String uploadPath;
+
+
+    @GetMapping("/find")
+    public ResponseEntity<BaseResponse<String>> getImage(@RequestParam Integer idx)
+    {
+        String result = imageService.find(idx);
+
+        return ResponseEntity.ok(BaseResponse.of(result, HttpStatus.OK));
     }
-
-
 }
