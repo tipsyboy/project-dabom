a80dc1d17685ea47650f7fc499c8f7d5897dded4
krh <fbsghksllove@gmail.com>
2025-08-18 14:30:28 +0900

    [Feat]video comment Swuagger

--- src/main/java/com/dabom/videocomment/model/dto/VideoCommentRegisterDto.java
+++ src/main/java/com/dabom/videocomment/model/dto/VideoCommentRegisterDto.java
@@ -1,10 +1,12 @@
 package com.dabom.videocomment.model.dto;
 
 import com.dabom.videocomment.model.entity.VideoComment;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class VideoCommentRegisterDto {
+    @Schema(description = "비디오 댓글", required = true, example = "댓글 내용")
     private String content;
     private String  username;
     private Integer videoIdx;
